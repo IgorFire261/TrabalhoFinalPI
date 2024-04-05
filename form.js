7ba8a5ad8dd586f4ab5cbb8f9a95e67051c9691f
IgorFire261 <igor.marizcunha@ufu.br>
2024-04-04 23:50:22 -0300

    Co-authored-by: dudamendesmav@gmail.com <dudamendesmav@gmail.com>

--- form.js
+++ form.js
@@ -4,20 +4,19 @@ const spanErrorName = document.getElementById("name-error");
 const inputEmail = document.getElementById("email");
 const spanErrorEmail = document.getElementById("email-error");
 
-const inputsNota = document.getElementsByName("nota");
 const spanErrorNotas = document.getElementById("nota-error");
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
 const inputMessage = document.getElementById("message");
 const spanErrorMessage = document.getElementById("message-error");
 
-const inputStudent = document.getElementById("student");
-const inputContact = document.getElementById("option");
+const inputRadio = document.getElementsByName("radio");
 
 const sendButton = document.getElementById("send-button");
 sendButton.onclick = () => {
     let error = false;
-    [ spanErrorName, spanErrorCPF, spanErrorEmail ].forEach((e) => e.innerHTML = "");
-    [ inputName, inputCPF, inputEmail ].forEach((e) => e.style.border = "none")
+    [ spanErrorName, spanErrorEmail ].forEach((e) => e.innerHTML = "");
+    [ inputName, inputEmail ].forEach((e) => e.style.border = "none")
     inputMessage.style.border = "solid 1px black"
     spanErrorNotas.style.display = "none";
     
@@ -29,6 +28,7 @@ sendButton.onclick = () => {
     }
     
     const emailContent = inputEmail.value;
+
     if(emailContent.length == 0 || emailContent[0] == "@" || !emailContent.includes(".") || !emailContent.includes("@") || (emailContent.indexOf(".") - emailContent.indexOf("@")) < 2) {
         spanErrorEmail.textContent = "O @ do email não pode ser o primeiro caractere e precisam existir dois caracteres entre o '@' e o '.'.";
         inputEmail.style.border = "solid red 1px"
@@ -42,25 +42,38 @@ sendButton.onclick = () => {
     }
     
     let checked = false;
-    inputsNota.forEach((e) => { if(e.checked) checked = true })
+    checkboxes.forEach((e) => { if(e.checked) checked = true })
     if(!checked) {
-        spanErrorNotas.innerHTML = 'Nenhuma atividade marcada!<button id="close-alert-button" type="button" onclick>X</button>'
+        spanErrorNotas.innerHTML = 'Nenhum experiencia marcada!<button id="close-alert-button" type="button" onclick>X</button>'
         spanErrorNotas.style.display = "flex";
         document.getElementById("close-alert-button").onclick = () => spanErrorNotas.style.display = "none"
     }
     
-    if(!error) {
-        const notas = [];
-        inputsNota.forEach((e) => { if(e.checked) notas.push(e.value) })
 
+    if (!error) {
+        const notas = [];
+        checkboxes.forEach((e) => { if (e.checked) notas.push(e.value) });
+    
         const confirmModal = new bootstrap.Modal('#confirm-modal');
         document.getElementById("name-confirm").textContent = inputName.value;
         document.getElementById("email-confirm").textContent = inputEmail.value;
-        document.getElementById("student-confirm").textContent = inputStudent.checked ? "Sim" : "Não";
+        let checkedRadio = "";
+        inputRadio.forEach(radio => {
+            if (radio.checked) {
+                checkedRadio = radio.value;
+            }
+        });
+        document.getElementById("radio-confirm").textContent = checkedRadio;
+        
+        for (let i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            console.log(checkboxes[i].value);
+         }
+        }
         document.getElementById("Notas-confirm").textContent = notas;
-        document.getElementById("contact-confirm").textContent = inputContact.value;
         document.getElementById("message-confirm").textContent = inputMessage.value;
         
-        confirmModal.show()
+        confirmModal.show();
     }
+    
 }
